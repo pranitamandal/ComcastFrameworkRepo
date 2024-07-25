60a14ad785b09bde6243ea3d83c64ae5dbe165e3
Admin <Admin@DESKTOP-1MPHE0I.mshome.net>
2024-07-25 16:00:37 +0530

    added product link in home page

--- SeleniumCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/HomePage.java
+++ SeleniumCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/HomePage.java
@@ -12,6 +12,12 @@ public class HomePage {
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
 	}
+	@FindBy(linkText="Products")
+	private WebElement productLink;
+	
+	public WebElement getProductLink() {
+		return productLink;
+	}
 	@FindBy(linkText="Organizations")
 	private WebElement orgLink;
 	
