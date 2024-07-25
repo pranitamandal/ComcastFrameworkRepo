56f22d45104765fa7530690789baab2e1d341f18
Admin <Admin@DESKTOP-1MPHE0I.mshome.net>
2024-07-25 17:18:48 +0530

    Product pom class added new ele2

--- SeleniumCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
+++ SeleniumCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
@@ -1,5 +1,12 @@
 package com.comcast.crm.objectrepositoryutility;
 
-public class Products {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class Products {
+	@FindBy(xpath = "//img[@alt='Create Product...']")
+	private WebElement createProductImgBtn;
+
+	@FindBy(name = "search")
+	private WebElement ele2;
 }
